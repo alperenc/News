1232266f679030ce5b16a0b7ee550d4b8b48be31
Alp Eren Can <alperenc@gmail.com>
2017-10-04 19:48:30 +0300

    add RecyclerView with a LinearLayoutManager

--- app/src/main/java/com/alperencan/news/android/NewsActivity.java
+++ app/src/main/java/com/alperencan/news/android/NewsActivity.java
@@ -1,13 +1,30 @@
 package com.alperencan.news.android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class NewsActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
     }
 }
